34b0d835746f2e3dc9eeb6c63a9d78005c32f222
idhla <34611950+idhla@users.noreply.github.com>
2019-03-01 01:28:23 +1000

    Update CoverProcessor.cs

--- Books/CoverProcessor.cs
+++ Books/CoverProcessor.cs
@@ -8,10 +8,12 @@ namespace Books
 {
    public class CoverProcessor
     {
+      //passing a delegate into method
         public void Process(string path, Action<Book> coverHandler)
         {
             var book = Book.Load(path);
-
+            
+           //client of this code will define what to use
             coverHandler(book);
 
                 book.Save();
